1853e943131ef0d24c0a7e747f0da31effdec820
chatard <jacques.chatard@gmail.com>
2018-12-07 20:35:43 +0100

    message

--- loadData.r
+++ loadData.r
@@ -1,3 +1,5 @@
+#loadData.r 
+
 if(!file.exists("exdata-data-household_power_consumption.zip")) {
   temp <- tempfile()
   download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
