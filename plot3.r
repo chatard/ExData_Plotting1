acea848294953a90f3eb19c881f92b81b43ddd5e
chatard <jacques.chatard@gmail.com>
2018-12-07 21:18:41 +0100

    message

--- plot3.r
+++ plot3.r
@@ -4,7 +4,7 @@ with(dataf,plot(timestamp, Sub_metering_1, type="l", xlab="", ylab="Energy sub m
 lines(timestamp,Sub_metering_2,col="red")
 lines(timestamp, Sub_metering_3,col="blue")
 legend("topright", col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),lty=c(1,1), lwd=c(1,1))
-dev.copy(png, file="plot3.png", width=480, height=480)
+dev.copy(png, file="plot3.png", width=560, height=560)
 dev.off()
 currentdir <- getwd()
 cat("plot3.png is in", currentdir)
